c9acc7df1061f1b11a7223197f8b6753195a788d
anacaroline-reis <anacaroline.reis.mg@gmail.com>
2024-01-04 19:36:47 -0300

    Desafio Classificação de Herói

--- desafio.js
+++ desafio.js
@@ -1,3 +1,42 @@
-let nome = "ana caroline";
-console.log (nome);
+let nome = "Estopinha"
+
+let expXP = 6500;
+
+let niveldoHeroi;
+
+
+if (niveldoHeroi < 1000) {
+ niveldoHeroi = "Ferro"}
+
+  else if (expXP >= 1001 && expXP <= 2000) {
+
+  niveldoHeroi = "Bronze"}
+
+  else if (expXP >= 2001 && expXP <= 5000) {
+
+  niveldoHeroi = "Prata"}
+
+ else if (expXP >= 5001 && expXP <= 7000) {
+
+  niveldoHeroi = "Ouro"}
+
+ else if (expXP >= 7001 && expXP <= 8000) {
+
+  niveldoHeroi = "Platina"}
+
+ else if (expXP >= 8001 && expXP <= 9000) {
+
+  niveldoHeroi = "Ascendente"}
+
+ else if (expXP >= 9001 && expXP <= 10000) {
+
+  niveldoHeroi = "Imortal"}
+
+ else {niveldoHeroi = "Radiante"}
+
+console.log ("O Herói de nome " + nome + " está no nível " + niveldoHeroi)
+
+
+
+
 
