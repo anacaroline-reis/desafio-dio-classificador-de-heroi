f31b0031ebb5bbb6945f8b2100bd73ea4ae22ef9
Ana Caroline <155485538+anacaroline-reis@users.noreply.github.com>
2024-01-04 19:45:31 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -1,3 +1,5 @@
+// Desafio Elaborado visando criar uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, com base em dados previamente informados.
+
 let nome = "Estopinha"
 
 let expXP = 6500;
